70e6932237fcc885a36b92018a8a0c3eb5b3b61e
Karabo Makgoba <makgobask2002@gmail.com>
2022-07-29 15:49:39 +0200

    ✨Added missing components
    💄 Improved styling

--- src/pages/admin/event/EventList.js
+++ src/pages/admin/event/EventList.js
@@ -1,12 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
+import { observer } from "mobx-react-lite";
+
 import Title from "../../../components/Title";
 import SearchInput from "../../../components/SearchInput";
 import EventListComp from "../../../components/EventListComp";
+import { StyledToggle as Toggle, Option } from "../../../components/Toggle";
 
-const EventList = () => {
+const EventList = observer(() => {
+  const [screen, setScreen] = useState("all");
+
   return (
     <>
       <Title>Event List</Title>
+      <Toggle
+        OnClick={(e) => setScreen(e)}
+        width="300px"
+        options={["all", "upcoming", "rest"]}
+      >
+        <Option
+          background={screen === "all" ? "var(--darkpurple)" : "none"}
+          color={screen === "all" ? "white" : "black"}
+          onClick={() => setScreen("all")}
+        >
+          All
+        </Option>
+        <Option
+          background={screen === "upcoming" ? "var(--darkpurple)" : "none"}
+          color={screen === "upcoming" ? "white" : "black"}
+          onClick={() => setScreen("upcoming")}
+        >
+          Upcoming
+        </Option>
+        <Option
+          background={screen === "rest" ? "var(--darkpurple)" : "none"}
+          color={screen === "rest" ? "white" : "black"}
+          onClick={() => setScreen("rest")}
+        >
+          rest
+        </Option>
+      </Toggle>
       <SearchInput
         width="300px"
         type="search"
@@ -14,8 +46,16 @@ const EventList = () => {
         placeholder="Search Here"
         onChange={(e) => console.log(e.target.value)}
       />
-      <EventListComp />
+      {screen === "all" ? (
+        <EventListComp />
+      ) : screen === "Upcoming" ? (
+        <h2>Upcoming</h2>
+      ) : screen === "rest" ? (
+        <h2>NEW</h2>
+      ) : (
+        <></>
+      )}
     </>
   );
-};
+});
 export default EventList;
