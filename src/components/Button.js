17e0ef3f87ade2d003d1145db2aee7823332e8d5
Karabo Makgoba <makgobask2002@gmail.com>
2022-08-06 00:23:13 +0200

    ✨ Added 2nd new event screen
    💄 Edited Button radius

--- src/components/Button.js
+++ src/components/Button.js
@@ -25,7 +25,7 @@ export const StyledButton = styled.button`
   border: ${({ border }) => border || "none"};
   margin: 3px;
   cursor: pointer;
-  border-radius: 5px;
+  border-radius: 15px;
   color: ${({ color }) => color || "white"};
   text-decoration: none;
   background: ${({ background }) => background || "none"};
--- src/pages/admin/event/CreateEvent.js
+++ src/pages/admin/event/CreateEvent.js
@@ -1,93 +1,171 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
-import { GrImage } from "react-icons/gr";
+import { GrImage, GrFormNext, GrFormPrevious } from "react-icons/gr";
 import Input from "../../../components/Input";
 import Select from "../../../components/Select";
 import Title from "../../../components/Title";
 import image from "../../../assets/image.svg";
+import Button from "../../../components/Button";
+
 const CreateEvent = () => {
+  const [page, setPage] = useState("false");
+
   return (
     <div>
       <Title width="auto">Create New Event</Title>
-      <h3>1. Event Details</h3>
-      <Wrapper>
-        <Imagewrapper>
-          <label>Upload an Image</label>
-          <FileWrapper>
-            <Label>
-              <GrImage color="var(--grey)" size={80} />
-              <label>Drag and drop, or browse</label>
-            </Label>
-            <File
-              type="file"
-              id="single"
-              accept="image/*"
-              onChange={(e) => console.log(e.target.files)}
-            />
-          </FileWrapper>
-        </Imagewrapper>
-        <FormWrapper>
-          <label>Name</label>
-          <Input className="textInput" type="text" width="auto" />
-          <label>Description</label>
-          <textarea
-            style={{
-              width: "auto",
-              height: "100px",
-              margin: "10px 0px",
-              borderRadius: "6px",
-            }}
-          />
-          <DetailsWrapper>
-            <HostDetails>
-              <label>Host / Organization </label>
+      {page ? (
+        <EventDetails>
+          <h3>1. Event Details</h3>
+          <Wrapper>
+            <Imagewrapper>
+              <label>Upload an Image</label>
+              <FileWrapper>
+                <Label>
+                  <GrImage color="var(--grey)" size={80} />
+                  <label>Drag and drop, or browse</label>
+                </Label>
+                <File
+                  type="file"
+                  id="single"
+                  accept="image/*"
+                  onChange={(e) => console.log(e.target.files)}
+                />
+              </FileWrapper>
+            </Imagewrapper>
+            <FormWrapper>
+              <label>Name</label>
               <Input className="textInput" type="text" width="auto" />
-              <label>Venue</label>
-              <Select
-                name="venue"
-                onChange={(e) => console.log(e.target.value)}
-                options={[
-                  { id: "1", name: "Event 1" },
-                  { id: "2", name: "Event 2" },
-                  { id: "3", name: "Event 3" },
-                ]}
+              <label>Description</label>
+              <textarea
+                style={{
+                  width: "auto",
+                  height: "100px",
+                  margin: "10px 0px",
+                  borderRadius: "6px",
+                }}
               />
-              <label>Type</label>
-              <Select
-                name="type"
-                onChange={(e) => console.log(e.target.value)}
-                options={[
-                  { id: "1", name: "Event 1" },
-                  { id: "2", name: "Event 2" },
-                  { id: "3", name: "Event 3" },
-                ]}
-              />
-            </HostDetails>
-            <div>
-              <DateWrapper>
-                <label>Date and Time</label>
-                <label>Select a day</label>
-                <Input type="date" width="auto" />
-              </DateWrapper>
-              <TimeWrapper>
-                <span>
-                  <label>Starts at</label>
-                  <Input type="time" width="auto" />
-                </span>
-                <span>
-                  <label>Ends at</label>
-                  <Input type="time" width="auto" />
-                </span>
-              </TimeWrapper>
+              <DetailsWrapper>
+                <HostDetails>
+                  <label>Host / Organization </label>
+                  <Input className="textInput" type="text" width="auto" />
+                  <label>Venue</label>
+                  <Select
+                    name="venue"
+                    onChange={(e) => console.log(e.target.value)}
+                    options={[
+                      { id: "1", name: "Event 1" },
+                      { id: "2", name: "Event 2" },
+                      { id: "3", name: "Event 3" },
+                    ]}
+                  />
+                  <label>Type</label>
+                  <Select
+                    name="type"
+                    onChange={(e) => console.log(e.target.value)}
+                    options={[
+                      { id: "1", name: "Event 1" },
+                      { id: "2", name: "Event 2" },
+                      { id: "3", name: "Event 3" },
+                    ]}
+                  />
+                </HostDetails>
+                <div>
+                  <DateWrapper>
+                    <label>Date and Time</label>
+                    <label>Select a day</label>
+                    <Input type="date" width="auto" />
+                  </DateWrapper>
+                  <TimeWrapper>
+                    <span>
+                      <label>Starts at</label>
+                      <Input type="time" width="auto" />
+                    </span>
+                    <span>
+                      <label>Ends at</label>
+                      <Input type="time" width="auto" />
+                    </span>
+                  </TimeWrapper>
+                </div>
+              </DetailsWrapper>
+            </FormWrapper>
+            <button
+              style={{ background: "none", border: "none" }}
+              onClick={() => setPage(false)}
+            >
+              <GrFormNext color="var(--purple)" size={40} />{" "}
+            </button>
+          </Wrapper>
+        </EventDetails>
+      ) : (
+        <TicketDetails>
+          <h3>2. Ticket Details</h3>
+          <TicketWrapper>
+            <button
+              style={{ background: "none", border: "none" }}
+              onClick={() => setPage(true)}
+            >
+              <GrFormPrevious color="var(--purple)" size={40} />{" "}
+            </button>
+            <div style={{ width: "100%" }}>
+              <InputWrapper>
+                <div>
+                  <label>Type: </label>
+                  <form>
+                    <div>
+                      <input type="checkbox" id="Early" name="Early" />
+                      <label for="Early">Early Bird</label>
+                    </div>
+
+                    <div>
+                      <input type="checkbox" id="General" name="General" />
+                      <label for="General">General Admission</label>
+                    </div>
+                    <div>
+                      <input type="checkbox" id="VIP" name="VIP" />
+                      <label for="VIP">VIP</label>
+                    </div>
+                  </form>
+                </div>
+                <div style={{ display: "grid" }}>
+                  <label>Price: </label>
+                  <Input placeholder="R 0" width="120px" />
+                </div>
+                <div style={{ display: "grid" }}>
+                  <label>No. of Tickets: </label>
+                  <Input placeholder="0" width="120px" />
+                </div>
+              </InputWrapper>
+              <ButtonsWrapper>
+                <Button
+                  color="var(--darkpurple)"
+                  border="1px solid var(--darkpurple)"
+                  hover="var(--darkpurple)"
+                  width="115px"
+                  onClick={() => alert("cancel")}
+                >
+                  Cancel
+                </Button>
+                <Button
+                  color="white"
+                  border="1px solid var(--darkpurple)"
+                  width="115px"
+                  hover="var(--purple)"
+                  background="var(--darkpurple)"
+                  onClick={() => alert("Create")}
+                >
+                  Create
+                </Button>
+              </ButtonsWrapper>
             </div>
-          </DetailsWrapper>
-        </FormWrapper>
-      </Wrapper>
+          </TicketWrapper>
+        </TicketDetails>
+      )}
     </div>
   );
 };
 export default CreateEvent;
 
+const EventDetails = styled.div``;
 const Wrapper = styled.div`
   display: flex;
   padding: 10px;
@@ -167,4 +245,26 @@ const Label = styled.div`
   display: grid;
   place-items: center;
   position: absolute;
+`;
+
+const TicketDetails = styled.div``;
+const TicketWrapper = styled.div`
+  display: flex;
+  height: 100%;
+  justify-content: space-between;
+`;
+
+const InputWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  height: 250px;
+  width: 70%;
+`;
+
+const ButtonsWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  height: 250px;
+  align-items: center;
 `;
