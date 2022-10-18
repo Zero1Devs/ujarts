295e426c80acd99f7e8470fa50d568cde41c250c
Vivaldo Gaston <vivaldo2009@live.com.pt>
2022-10-18 12:08:05 +0200

    🐛Bug fixes

--- src/pages/booking/Rsvp.js
+++ src/pages/booking/Rsvp.js
@@ -5,17 +5,21 @@ import { useEventPresenter } from "../admin/event/presenter";
 import { DownloadPhoto } from "../../util/DownloadPhoto";
 import { Info, StyledEventSummary } from "../../components/EventSummary";
 import Button from "../../components/Button";
-import RsvpForm from "../../components/Forms/RsvpForm";
+//import RsvpForm from "../../components/Forms/RsvpForm";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 import image from "../../assets/image.svg";
 import CustomerLayout from "../../layouts/CustomerLayout";
+import { useBookingPresenter } from "./presenter";
+import { Radio } from "./Checkout";
+
 const Rsvp = observer(() => {
+  const [checked, setChecked] = useState("");
   let params = useParams();
 
   let eventId = params.event;
   const { gridEvents } = useEventPresenter;
-
+  const { setFormValue } = useBookingPresenter;
   const [url, setUrl] = useState(image);
   useEffect(() => {
     gridEvents.length > 0 &&
@@ -36,7 +40,87 @@ const Rsvp = observer(() => {
         </Cover>
         <div style={{ width: "76%" }}>
           <h1>RSVP Tickets</h1>
-          <RsvpForm />
+
+          <div
+            style={{
+              width: "40%",
+              marginLeft: "10%",
+              marginTop: "10%",
+            }}
+          >
+            <h3>Provide Your details</h3>
+            <label>Your name</label>
+            <Input
+              type="text"
+              id="name"
+              name="name"
+              className="textInput"
+              onChange={console.log("change name")}
+            />
+            <label>Your email Address</label>
+            <Input
+              type="text"
+              id="name"
+              name="email"
+              className="textInput"
+              onChange={(e) => console.log(e.target.value)}
+            />
+            <label>Confirm email Address</label>
+            <Input
+              type="text"
+              id="name"
+              name="confirm_email"
+              className="textInput"
+              onChange={(e) => console.log(e.target.value)}
+            />
+            <label>Your Telephone Number</label>
+            <Input
+              type="text"
+              id="name"
+              name="Telnumber"
+              className="textInput"
+              onChange={(e) => console.log(e.target.value)}
+            />
+            <label>Communication Method</label>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "space-between",
+                width: "50%",
+              }}
+            >
+              <Radio>
+                <label htmlFor="sms">SMS</label>
+                <input
+                  id="sms"
+                  name="communication_method"
+                  value={"sms"}
+                  checked={checked === "sms"}
+                  onChange={(e) => {
+                    setFormValue(e);
+                    setChecked(e.target.value);
+                    console.log(e.target.name);
+                  }}
+                  type={"radio"}
+                />
+              </Radio>
+              <Radio>
+                <label htmlFor="email"> Email</label>
+                <input
+                  id="email"
+                  name="communication_method"
+                  value={"email"}
+                  checked={checked === "email"}
+                  onChange={(e) => {
+                    setFormValue(e);
+                    setChecked(e.target.value);
+                    console.log(e.target.name);
+                  }}
+                  type={"radio"}
+                />
+              </Radio>
+            </div>
+          </div>
         </div>
         <div
           style={{
