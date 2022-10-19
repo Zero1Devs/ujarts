466dfd270df69fd3f46fc07e379a3295676e7e7a
Zacharia Siphoro <zachariasiphoro@gmail.com>
2022-10-19 10:50:34 +0200

    🐛 Path fix
    Co-Authored-By: Vivaldo Gaston <66387304+vivaldogaston@users.noreply.github.com>

--- src/layouts/FrontOfHouseLayOut.js
+++ src/layouts/FrontOfHouseLayOut.js
@@ -2,7 +2,8 @@ import React, { useEffect } from "react";
 import ujlogo from "../assets/ujLogo.jpg";
 import "../styles/index";
 import * as Icon from "react-icons/fi";
-import { BsList, BsListTask, BsCashCoin } from "react-icons/bs";
+import { BsList, BsListTask, BsCashCoin, BsBack } from "react-icons/bs";
+import { BiArrowBack } from "react-icons/bs";
 import { NavLink } from "react-router-dom";
 import { useAdminPresenter } from "../pages/admin/auth/presenter";
 import { useUserStore } from "../stores/userStore";
@@ -55,6 +56,22 @@ const FrontOfHouseLayout = (props) => {
           to="/admin/cash-booking"
         >
           <BsCashCoin color="white" size={25} />
+        </NavLink>
+        <NavLink
+          className="navLink"
+          style={({ isActive }) => {
+            return {
+              background: isActive ? "var(--darkpurple)" : "",
+              borderLeft: isActive
+                ? "solid 5px white"
+                : "solid 5px transparent",
+              borderTopLeftRadius: "2px",
+              borderBottomLeftRadius: "2px",
+            };
+          }}
+          to="/admin/events"
+        >
+          <BiArrowBack color="white" size={25} />
         </NavLink>
 
         <div onClick={() => logout()} className="navLink">
